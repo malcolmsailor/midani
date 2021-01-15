33dae5c0483700b9421c6d102a866845d6081cd0
Malcolm Sailor <malcolm.sailor@gmail.com>
2021-01-15 09:38:41 -0600

    implemented test_sample_settings.sh hook

--- tests/test_sample_settings.sh
+++ tests/test_sample_settings.sh
@@ -1,11 +1,39 @@
 #!/bin/sh
 
-# Run all `sample_settings`. Any extra flags (e.g., --test) are passed through
+# Run all `sample_settings`.
+# Should be run with either "--frames" or "--videos" as first argument
+# Any extra flags (e.g., --test) are passed through
 # to midani.
 
-BASEDIR=$(dirname "$0")
+function usage() {
+    echo 'First argument must be either "--frames" or "--videos"'
+    echo 'Subsequent arguments are passed through to midani'
+    exit 1
+}
 
-for settings in "${BASEDIR}"/../sample_settings/settings*.py
+case "$1" in
+    --frames ) frames=1
+        ;;
+    --videos ) frames=0
+        ;;
+    * ) usage
+esac
+
+MIDANI_DIR=$(dirname "$0")/..
+
+for settings in "${MIDANI_DIR}"/sample_settings/settings*.py
 do
-    python3 "${BASEDIR}/../midani.py" --settings "$settings" "$@"
+    if [[ $frames -eq 1 ]]
+    then
+        python3 "${MIDANI_DIR}/midani.py" --settings "$settings" \
+            "${MIDANI_DIR}/tests/test_settings/frame_settings.py" \
+            --frames 0,2,4 "${@:2}"
+        if [[ $? -ne 0 ]]
+        then
+            echo Error with "$settings", aborting...
+            exit 1
+        fi
+    else
+        python3 "${MIDANI_DIR}/midani.py" --settings "$settings" "${@:2}"
+    fi
 done
---
+++ tests/test_settings/frame_settings.py
@@ -0,0 +1,3 @@
+{
+    "output_dirname": "tests/test_out/frames",
+}
