196d3f29954212bac917541fe6cf9606f8e0442c
Malcolm Sailor <malcolm.sailor@gmail.com>
2021-01-30 09:53:47 -0500

    pylint ignore directories w/ help of src/third_party/pylint_ignore.py

---
+++ src/third_party/pylint_ignore.py
@@ -0,0 +1,23 @@
+# from https://github.com/PyCQA/pylint/issues/2686#issuecomment-621927895
+
+from pylint.utils import utils
+
+
+class PylintIgnorePaths:
+    def __init__(self, *paths):
+        self.paths = paths
+        self.original_expand_modules = utils.expand_modules
+        utils.expand_modules = self.patched_expand
+
+    def patched_expand(self, *args, **kwargs):
+        result, errors = self.original_expand_modules(*args, **kwargs)
+
+        def keep_item(item):
+            if any(1 for path in self.paths if item['path'].startswith(path)):
+                return False
+
+            return True
+
+        result = list(filter(keep_item, result))
+
+        return result, errors
