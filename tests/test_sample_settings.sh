263c1761c9105edad02481f7062b0709ef578511
Malcolm Sailor <malcolm.sailor@gmail.com>
2021-01-30 09:24:45 -0500

    refactored test_sample_settings.sh a little

--- tests/test_sample_settings.sh
+++ tests/test_sample_settings.sh
@@ -5,10 +5,33 @@
 # Any extra flags (e.g., --test) are passed through
 # to midani.
 
-function usage() {
+usage() {
     echo 'First argument must be either "--frames" or "--videos"'
     echo 'Subsequent arguments are passed through to midani'
     exit 1
+}
+
+MIDANI_DIR=$(dirname "$0")/..
+TEMP_OUT="${MIDANI_DIR}"/tests/.temp_sample_settingsTEMP_OUT
+
+# echo_and_run after https://stackoverflow.com/a/12240862/10155119
+echo_and_run() {
+    echo "\$ $*"
+    "$@" >& "$TEMP_OUT"
+}
+
+try_to_run() {
+    echo_and_run "$@"
+    if [[ $? -ne 0 ]]
+    then
+        cat "$TEMP_OUT"
+        echo Error running "$@", aborting
+        rm "$TEMP_OUT"
+        exit 1
+    else
+        rm "$TEMP_OUT"
+        echo ===============================================================
+    fi
 }
 
 case "$1" in
@@ -19,21 +42,17 @@ case "$1" in
     * ) usage
 esac
 
-MIDANI_DIR=$(dirname "$0")/..
+
 
 for settings in "${MIDANI_DIR}"/sample_settings/settings*.py
 do
     if [[ $frames -eq 1 ]]
     then
-        python3 "${MIDANI_DIR}/midani.py" --settings "$settings" \
+        try_to_run python3 "${MIDANI_DIR}/midani.py" --settings "$settings" \
             "${MIDANI_DIR}/tests/test_settings/frame_settings.py" \
             --frames 0,2,4 "${@:2}"
-        if [[ $? -ne 0 ]]
-        then
-            echo Error with "$settings", aborting...
-            exit 1
-        fi
     else
-        python3 "${MIDANI_DIR}/midani.py" --settings "$settings" "${@:2}"
+        try_to_run python3 "${MIDANI_DIR}/midani.py" \
+            --settings "$settings" "${@:2}"
     fi
 done
